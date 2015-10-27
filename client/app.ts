d2d71eeff75972d3b06df28e885f5f02c458bc2b
kuka <kukawski.lukasz@gmail.com>
2015-10-27 06:06:35 +0100

    0. Bootstrapping

---
+++ client/app.ts
@@ -0,0 +1,13 @@
+/// <reference path="../typings/angular2-meteor.d.ts" />
+
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+	selector: 'app'
+})
+@View({
+	templateUrl: 'client/app.html'
+})
+class Socially { }
+
+bootstrap(Socially);
